cc878931b229ef1d2abf596316e5f83b84440976
Andreas Bigger <abigger87@gmail.com>
2021-09-23 02:08:47 -0400

    :bug: css error :boom:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import { AppProps } from 'next/app'
+// import "../assets/css/color/green.css";
+// import "../assets/css/style.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
